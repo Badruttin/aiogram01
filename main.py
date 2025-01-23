97c5440aa06ebe5ce1195d2b356ed0f50d6ecd09
Badruttin <badruttin@gmail.com>
2025-01-23 15:55:05 +0300

    jan23_01_02

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from os import getenv
 from app.handlers import router
+from app.database.models import async_main
 
 
 
@@ -9,6 +10,7 @@ TOKEN = getenv("BOT_TOKEN")
 
 
 async def main(): 
+    await async_main()
     bot = Bot(TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
