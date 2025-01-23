70e641e5970c36d6ea603a6fc9463ac6d81bf254
Badruttin <badruttin@gmail.com>
2025-01-23 12:28:28 +0300

    jan23_01_ready_without_db

--- app/keyboards.py
+++ app/keyboards.py
@@ -10,4 +10,14 @@ main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],
                                      resize_keyboard=True,
                                      input_field_placeholder='Выберите пункт меню')
 
-catalog = InlineKeyboardMarkup
+catalog = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Футболки', callback_data='t-shirt')],
+        [InlineKeyboardButton(text='кроссовки', callback_data='sneakers')],
+        [InlineKeyboardButton(text='Кепки', callback_data='cap')]
+        ])
+
+
+get_number = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Отправить номер', request_contact=True)]
+],
+resize_keyboard=True)
