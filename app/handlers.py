315d0c1f1587071b14fc905285956d269e2dc6f0
Badruttin <badruttin@gmail.com>
2025-01-29 19:25:26 +0300

    jan_29_01

--- app/handlers.py
+++ app/handlers.py
@@ -5,6 +5,7 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
+import app.database.requests as rq
 
 router = Router()
 
@@ -16,8 +17,9 @@ class Register(StatesGroup):
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
+    await rq.set_user(message.from_user.id)
     await message.answer('Привет!', reply_markup=kb.main)
-    await message.reply('Как дела?')
+    
 
 @router.message(Command('help'))
 async def cmd_help(message: Message):
@@ -25,11 +27,23 @@ async def cmd_help(message: Message):
 
 @router.message(F.text == 'Каталог')
 async def cmd_catalog(message: Message):
-    await message.answer('Вы выбрали каталог', reply_markup= kb.catalog)
+    await message.answer('Выберите категорию товара', reply_markup= await kb.categories())
 
-@router.callback_query(F.data == 't-shirt')
-async def t_shirt(callback: CallbackQuery):
-    await callback.answer('Вы выбрали категорию футболки')
+@router.callback_query(F.data.startswith ('category_'))
+async def category(callback: CallbackQuery):
+    await callback.answer('Вы выбрали категорию')
+    await callback.message.answer('Выберите товар по категории', 
+                                  reply_markup= await kb.items(callback.data.split('_')[1]))
+
+
+@router.callback_query(F.data.startswith ('item_'))
+async def item(callback: CallbackQuery):
+    item_data = await rq.get_item(callback.data.split('_')[1])
+    await callback.answer('Вы выбрали товар')
+    await callback.message.answer(f'Название: {item_data.name}\nОписание: {item_data.description}', 
+                                  reply_markup= await kb.items(callback.data.split('_')[1]))
+
+
 
 @router.message(Command('register'))
 async def register(message: Message, state : FSMContext):
